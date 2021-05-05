8a2541a7a26c9d4b32b0ab8b01f44cb44f1e0121
Robin Kamo <robin.kamo@codic.se>
2021-05-05 11:52:37 +0200

    Add project files.

---
+++ Codic Education Discrod Bot/Program.cs
@@ -0,0 +1,34 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+using System;
+using System.Threading.Tasks;
+
+namespace Codic_Education_Discrod_Bot
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+
+            var discordClient = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "ODM5NDMzNTg4ODM5MDIyNjEy.YJJlcw.JxpDGZscTxF155eVlv_lMCrrRfA",
+                TokenType = TokenType.Bot
+            });
+
+            discordClient.MessageCreated += OnMessageCreated;
+
+            await discordClient.ConnectAsync();
+            await Task.Delay(-1);
+        }
+        //https://discordapp.com/oauth2/authorize?client_id=839433588839022612&scope=bot&permissions=0
+        private static async Task OnMessageCreated(MessageCreateEventArgs user)
+        {
+            if(string.Equals(user.Message.Content, "hjälp", StringComparison.OrdinalIgnoreCase))
+            {
+                await user.Message.RespondAsync(user.Message.Author.Username + " ge inte upp, det blir bättre!");
+            }
+        }
+
+    }
+}
